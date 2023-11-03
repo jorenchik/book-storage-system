08d89ccfddba31e6de37f31e02b58aecac008ae0
jorenchik <jorens.stekels@gmail.com>
2023-11-03 21:34:48 +0200

    menu option dict (map)

--- main.py
+++ main.py
@@ -8,8 +8,40 @@ book: Dict = {"title": "Pride and prejudice",
               "quantity_in_stock": 5
               }
 
+
+def add_book_option():
+    pass
+
+
+def search_book_option():
+    pass
+
+
+def search_book_by_arg_option():
+    pass
+
+
+def list_books_option():
+    pass
+
+
+def delete_book_option():
+    pass
+
+
 if __name__ == "__main__":
-    books = [book, deepcopy(book)]
+
+    books = [book]
+
+    menu_options = {'Add book': add_book_option,
+                    'Search a book': search_book_by_arg_option,
+                    'Search a book (ISBN)': search_book_option,
+                    'List books': list_books_option,
+                    'Delete book': delete_book_option
+                    }
+
+    for i, val in enumerate(menu_options):
+        print(i, val, sep=": ")
 
     for i, book in enumerate(books):
         print(f"Book #{i}:")
