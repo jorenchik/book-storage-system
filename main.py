f57638e61daa77c02dfb7b3655aadbe795718c72
jorenchik <jorens.stekels@gmail.com>
2023-11-03 18:42:02 +0200

    basic data structure

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
+from typing import Dict
+from copy import deepcopy
+
+book: Dict = {"title": "Pride and prejudice",
+              "author": "John Doe",
+              "ISBN": 12435324242432,
+              "price": 12.2,
+              "quantity_in_stock": 5
+              }
+
 if __name__ == "__main__":
-    pass
+    books = [book, deepcopy(book)]
+
+    for i, book in enumerate(books):
+        print(f"Book #{i}:")
+        for key, val in book.items():
+            print(key, val, sep=": ")
