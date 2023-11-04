5d34fd9c2e4723f9cf05b4b7778a2ad0582cbe8b
jorenchik <jorens.stekels@gmail.com>
2023-11-04 12:37:09 +0200

    separate menu loop logic

--- src/main.py
+++ src/main.py
@@ -1,17 +1,7 @@
 from typing import Dict
-import controller
+import menu
 
 
 if __name__ == "__main__":
 
-    menu_options = {'Exit': controller.exit_option,
-                    'Add book': controller.add_book_option,
-                    'Search a book': controller.search_book_by_arg_option,
-                    'Search a book (ISBN)': controller.search_book_option,
-                    'List books': controller.list_books_option,
-                    'Delete book': controller.delete_book_option}
-
-    while True:
-        controller.menu_option(menu_options)
-        option_index = int(input(">"))
-        list(menu_options.values())[option_index]()
+    menu.main_loop()
---
+++ src/menu.py
@@ -0,0 +1,13 @@
+import controller
+
+menu_options = {'Exit': controller.exit_option,
+                'Add book': controller.add_book_option,
+                'Search a book': controller.search_book_by_arg_option,
+                'Search a book (ISBN)': controller.search_book_option,
+                'List books': controller.list_books_option,
+                'Delete book': controller.delete_book_option}
+
+while True:
+    controller.menu_option(menu_options)
+    option_index = int(input(">"))
+    list(menu_options.values())[option_index]()
