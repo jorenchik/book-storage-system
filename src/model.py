21025151b341d62757ced5ab4c2db9cc803d6cef
jorenchik <jorens.stekels@gmail.com>
2023-11-04 17:25:41 +0200

    add validation errors

--- src/model.py
+++ src/model.py
@@ -5,6 +5,8 @@ class Book:
     price: float
     quantity_in_stock: int
 
+    LENGTH_ERROR = "{ATTR_NAME} should contain between {MIN_LENGTH}-{MAX_LENGTH} symbols"
+    REQUIRED_ERROR = "{ATTR_NAME} is required"
 
     __slots__ = ["isbn", "title", "author", "price", "quantity_in_stock"]
 
