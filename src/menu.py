5d7c757b09424c449f3fd267e893c3fad3accafb
jorenchik <jorens.stekels@gmail.com>
2023-11-05 11:31:45 +0200

    restructured menu

--- src/menu.py
+++ src/menu.py
@@ -1,33 +1,53 @@
-from typing import Dict
 from controller import BookController
 
 
 class Menu:
-    menu_options: Dict
+    menu_options: dict
     book_controller: BookController
 
     def __init__(self):
         self.book_controller = BookController()
         self.menu_options = {
-            "Exit": self.book_controller.exit_option,
-            "Add book": self.book_controller.add_book_option,
-            "Search a book": self.book_controller.search_book_by_arg_option,
-            "Search a book (ISBN)":
-            self.book_controller.search_book_isbn_option,
-            "List books": self.book_controller.list_books_option,
-            "Delete book": self.book_controller.delete_book_option,
+            "0": {
+                "name": "Exit",
+                "function": self.book_controller.exit_option
+            },
+            "1": {
+                "name": "Add book",
+                "function": self.book_controller.add_book_option
+            },
+            "2": {
+                "name": "Search a book",
+                "function": self.book_controller.search_book_by_arg_option
+            },
+            "3": {
+                "name": "Search a book (ISBN)",
+                "function": self.book_controller.search_book_isbn_option
+            },
+            "4": {
+                "name": "List books",
+                "function": self.book_controller.list_books_option
+            },
+            "5": {
+                "name": "Delete book",
+                "function": self.book_controller.delete_book_option
+            },
+            "?": {
+                "name": "Help",
+                "function": self.print_menu_cli
+            },
         }
 
     def menu_loop(self):
         while True:
-            self.print_menu_cli(self.menu_options)
-            option_index = int(input(">"))
-            list(self.menu_options.values())[option_index]()
+            option = input("> ")
+            print(self.menu_options.get(option)["function"])
+            self.menu_options.get(option)["function"]()
 
-    def menu_option(self, menu_options):
+    def menu_option(self):
         # TODO: choose show behaviour
         self.print_menu_cli(self.menu_options)
 
-    def print_menu_cli(self, menu_options):
-        for i, val in enumerate(menu_options):
-            print(i, val, sep=": ")
+    def print_menu_cli(self):
+        for key, val in self.menu_options.items():
+            print(key, val["name"], sep=": ")
