6420b7a030ae944851bc6957dcafd21bbc3b45a0
jorenchik <jorens.stekels@gmail.com>
2023-11-08 18:09:01 +0200

    Validation order

--- src/book_inventory/controllers.py
+++ src/book_inventory/controllers.py
@@ -10,9 +10,9 @@ class BookController:
         self.inventory.load_json_from_storage()
 
     def create(self, *kwargs):
-        book = Book(*kwargs)
         if "" in kwargs:
             raise TypeError("Every field should be filled")
+        book = Book(*kwargs)
         if not self.inventory.check_unique_isbn(book.isbn):
             raise TypeError("This ISBN already exists")
         self.inventory.books.append(book)
